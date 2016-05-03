2a948cb59bf43e3c544f6c85c625feaaa1a9109d
Zachary Spector <zacharyspector@gmail.com>
2016-05-03 16:53:19 -0400

    Create the basic characters

--- world.py
+++ world.py
@@ -1,4 +1,6 @@
 def install(engine):
     @engine.function
     def __init__(engine):
-        pass
+        player = engine.new_character('player')
+        phys = engine.new_character('physical')
+        soc = engine.new_character('social')
