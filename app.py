e8f45c3e7a2ce3c04804b84a20ca07b33b5fa0fa
Zachary Spector <zacharyspector@gmail.com>
2016-05-08 00:58:53 -0400

    Move all the kv to a dedicated file for maximum laziness

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from kivy.lang import Builder
 from kivy.properties import BooleanProperty, ObjectProperty
 
 from ELiDE.game import GameScreen, GameApp
@@ -26,44 +25,3 @@ class DunUniPlayView(GameScreen):
 
 class DunUniApp(GameApp):
     modules = ['util', 'emotion', 'world']
-    name = 'DunUni'
-
-
-Builder.load_string("""
-#: import Board ELiDE.board.Board
-<DunUniPlayView>:
-    BoxLayout:
-        BoxLayout:
-            id: buttons
-            orientation: 'vertical'
-            size_hint_x: 0.3
-            Button:
-                id: go2class
-                disabled: not root.class_time
-                text: "Go to class"
-                on_release: root.go_to_class()
-            Button:
-                id: sleep
-                disabled: not root.sleepy
-                text: "Sleep"
-                on_release: root.go_to_sleep()
-            Button:
-                id: eat
-                disabled: not root.hungry
-                text: "Eat"
-                on_release: root.eat_food()
-            Button:
-                id: soc
-                disabled: not root.people_present
-                text: "Socialize"
-                on_release: root.socialize()
-        Board:
-            id: board
-            size_hint_x: 0.6
-            engine: root.engine
-            character: root.character
-<Screens>:
-    DunUniPlayView:
-        name: 'play'
-        character: self.engine.character['player']
-""")
