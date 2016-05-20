48ef7ca3041e2b04f246b9739883f7fc99acd0c5
Zachary Spector <zacharyspector@gmail.com>
2016-05-20 16:13:58 -0400

    Set wallpaper

--- world.py
+++ world.py
@@ -2,5 +2,6 @@ def install(engine):
     @engine.method
     def game_start(engine):
         player = engine.new_character('player')
+        player.stat['wallpaper'] = 'uncle_mugen_tomb.jpg'
         phys = engine.new_character('physical')
         soc = engine.new_character('social')
