9a37f33e5b07fbbd130af817fc9b8df0f7aa8215
Zachary Spector <zacharyspector@gmail.com>
2016-05-22 19:59:19 -0400

    Implement behaviors for the 4 starting buttons

--- app.py
+++ app.py
@@ -11,16 +11,44 @@ class DunUniPlayView(GameScreen):
     people_present = BooleanProperty()
 
     def go_to_class(self, *args):
-        pass
+        me = self.character.avatar['physical']
+        classroom = self.engine.character['physical'].place['classroom']
+        me.travel_to(classroom)
+        while me.location != classroom:
+            self.engine.next_tick()
 
     def go_to_sleep(self, *args):
-        pass
+        myroom = self.character.stat['room']
+        me = self.character.avatar['physical']
+        if me.location != myroom:
+            me.travel_to(myroom)
+            while me.location != myroom:
+                self.engine.next_tick()
+        bed = self.character.stat['bed']
+        me.location = bed
+        self.character.stat['conscious'] = False
+        for i in range(8):
+            self.engine.next_tick()
+        self.character.stat['conscious'] = True
 
     def eat_food(self, *args):
-        pass
+        cafeteria = self.engine.character['physical'].place['cafeteria']
+        me = self.character.avatar['physical']
+        if me.location != cafeteria:
+            me.travel_to(cafeteria)
+            while me.location != cafeteria:
+                self.engine.next_tick()
+        self.character.stat['eating'] = True
+        self.engine.next_tick()
+        self.character.stat['eating'] = False
 
     def socialize(self, *args):
-        pass
+        peeps = [thing for thing in self.character.avatar['physical'].contents() if thing.user]
+        self.character.stat['talking_to'] = self.engine.choice(peeps).user
+        self.character.stat['talking_to'].stat['talking_to'] = self.character
+        self.engine.next_tick()
+        self.character.stat['talking_to'].stat['talking_to'] = None
+        self.character.stat['talking_to'] = None
 
 
 class DunUniApp(GameApp):
