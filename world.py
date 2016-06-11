f8c89149045a55926159bde14c1a7c17595a23a6
Zachary Spector <zacharyspector@gmail.com>
2016-06-11 18:05:32 -0400

    Add a character representing the player

--- world.py
+++ world.py
@@ -7,13 +7,13 @@ def install(engine):
         # dorm 1
         rooms = [
             phys.new_place('room{}'.format(n), _x=0.1, _y=0.1+n/10)
-            for n in range(1, 7)
+            for n in range(1, 8)
         ]
         lounge = phys.new_place('lounge1', _x=0.2, _y=0.2)
         for room in rooms:
             lounge.two_way(room)
         for n, student in enumerate((
-                'sam', 'kylie', 'jeff', 'catt', 'jim', 'meredith'
+                'sam', 'kylie', 'jeff', 'catt', 'jim', 'meredith', 'player'
         )):
             engine.make_person(
                 student, rooms[n], engine.character['social']  # everybody knows everyone
